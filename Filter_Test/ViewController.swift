6b57881dd02352be99aea6b4ed7fa7b3910ad6fb
Igor Silaev <cokturqi55@mail.ru>
2020-09-16 14:51:25 +0300

    Work with player

--- Filter_Test/ViewController.swift
+++ Filter_Test/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - Public properties
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    
+    // MARK: - Private properties
+    private var videoURL: URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func tapChooseButton(_ sender: UIButton) {
+        openVideoGallery()
+    }
+    
+    // MARK: - Private methods
+    private func openVideoGallery() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        guard let mediaTypes = UIImagePickerController.availableMediaTypes(for: .savedPhotosAlbum) else { return }
+        picker.mediaTypes = mediaTypes
+        picker.mediaTypes = ["public.movie"]
+        
+        picker.allowsEditing = false
+        present(picker, animated: true, completion: nil)
+    }
+    
+    private func createPlayer(URL: URL?) {
+        guard let url = URL else { return }
+        let player = AVPlayer(url: url)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        self.addChild(playerController)
+    }
+    
 }
 
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL
+        self.dismiss(animated: true, completion: nil)
+        createPlayer(URL: videoURL)
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+    
+}
---
+++ Pods/Target Support Files/Pods-Filter_Test/Pods-Filter_Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Filter_Test : NSObject
+@end
+@implementation PodsDummy_Pods_Filter_Test
+@end
