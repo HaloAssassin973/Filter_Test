a59dee1f7d8484c10a6dff514fa416e925dccb75
Igor Silaev <cokturqi55@mail.ru>
2020-09-16 18:13:42 +0300

    Start animation

--- Filter_Test/DodecahedronVC.swift
+++ Filter_Test/DodecahedronVC.swift
@@ -15,12 +15,23 @@ class DodecahedronVC: UIViewController {
     @IBOutlet weak var sceneView: SCNView!
     
     override func viewDidLoad() {
-        // 2: Add camera node
+        
+        let scene = SCNScene(named: "Dodecahedron.scn")
+        let node = (scene?.rootNode.childNodes.first)!
+        let action = SCNAction.rotate(by: 360 * CGFloat(Double.pi / 180), around: SCNVector3(x:0, y:1, z:0), duration: 3)
+        let repeatAction = SCNAction.repeatForever(action)
+        node.runAction(repeatAction)
+        node.position = SCNVector3(x: 0, y: 0, z: 0)
+        let defaultScene = SCNScene()
+        sceneView.scene = defaultScene
+        sceneView.scene?.rootNode.addChildNode(node)
+        
+        
+        //        sceneView.scene = scene
+        
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        // 3: Place camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 20)
-        // 4: Set camera on scene
+        cameraNode.position = SCNVector3(x: 0, y: 37, z: 0)
         sceneView.scene?.rootNode.addChildNode(cameraNode)
         
         // 5: Adding light to scene
@@ -29,25 +40,13 @@ class DodecahedronVC: UIViewController {
         lightNode.light?.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
         sceneView.scene?.rootNode.addChildNode(lightNode)
-
-        // 6: Creating and adding ambien light to scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light?.type = .ambient
-        ambientLightNode.light?.color = UIColor.darkGray
-        sceneView.scene?.rootNode.addChildNode(ambientLightNode)
         
-        let url = Bundle.main.url(forResource: "Dodecahedron", withExtension: "stl")
-        let asset = MDLAsset(url: url!)
-        asset.loadTextures()
-        let object = asset.object(at: 0)
-        
-        let node = SCNNode(mdlObject: object)
-       
-        node.position = SCNVector3(x: 0, y: 0, z: 0)
-        print(node.boundingBox)
-        
-        sceneView.scene?.rootNode.addChildNode(node)
+        //        // 6: Creating and adding ambien light to scene
+        //        let ambientLightNode = SCNNode()
+        //        ambientLightNode.light = SCNLight()
+        //        ambientLightNode.light?.type = .ambient
+        //        ambientLightNode.light?.color = UIColor.darkGray
+        //        sceneView.scene?.rootNode.addChildNode(ambientLightNode)
         
     }
 }
